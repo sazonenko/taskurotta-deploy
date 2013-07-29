86f3e75ade1531b8e0cae1ea3fab99f725302c56
sazonenko <jedy@fccland.ru>
2013-07-29 20:05:56 +0700

    config changed

--- run.sh
+++ run.sh
@@ -1,4 +1,1 @@
-java -agentpath:/opt/taskurotta/jprofiler/bin/linux-x64/libjprofilerti.so=port=8849,nowait -jar ../lib/wf-dropwizard-memory-0.2.0-SNAPSHOT.jar server config.yml
-
-
-
+java -jar ../lib/wf-dropwizard-oracle-0.2.0-SNAPSHOT.jar server ./config.yml
